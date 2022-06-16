0d69fd790de3f365eeb14494ca93a0feb990d538
youshkoff <youshkoff@yandex.ru>
2022-06-16 19:15:55 +0300

    добавлен четвертый файл

---
+++ sergey/table4.py
@@ -0,0 +1,12 @@
+#Пропишем и присвоим ряд
+list1 = [1, 5, 78, 36, -45, -3, 46]
+print ('list =', list1)
+#Выберем наименьшее
+num = min(list1)
+print('наименьшее число =', num)
+
+#Сортировка
+list1 = [1, 5, 78, 36, -45, -3, 46]
+print('list =', list1)
+list1.sort()
+print('smallest number =', list1)
