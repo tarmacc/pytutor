c42ef7e826e1b601f0ee001d401876f7519d92ab
youshkoff <youshkoff@yandex.ru>
2022-06-22 16:28:19 +0300

    Подправил первое

--- sergey/practice#1-1.py
+++ sergey/practice#1-1.py
@@ -1,12 +1,10 @@
 string1 = 'world'[::-1]
 print(string1)
 #срез 1
-
 def reverse_string1(l):
     return l[::-1]
     print(reverse_string1('world'))
 #срез 2
-
 for text in reversed('world'):
     print(text)
 #reverse
