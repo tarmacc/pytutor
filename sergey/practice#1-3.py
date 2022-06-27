b8ede22c5d80157da885ae454117334b0fc16392
youshkoff <youshkoff@yandex.ru>
2022-06-27 10:02:06 +0300

    *

--- sergey/practice#1-3.py
+++ sergey/practice#1-3.py
@@ -1,5 +1,5 @@
 string = 'жаргон падонкаф'
-for i, l in enumerate(string):#пронумирровали
+for i, l in enumerate(string):#пронумировали
     if i%2:#четные нечетныеgit 
         print(l.upper(), end='')
     else:
