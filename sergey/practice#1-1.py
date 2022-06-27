2e15ea6500472b44a7eaeae9b2623b8993ea3311
youshkoff <youshkoff@yandex.ru>
2022-06-27 17:56:00 +0300

    сохранение 1

--- sergey/practice#1-1.py
+++ sergey/practice#1-1.py
@@ -1,10 +1,12 @@
 string1 = 'world'[::-1]
 print(string1)
 #срез 1
+
 def reverse_string1(l):
     return l[::-1]
     print(reverse_string1('world'))
 #срез 2
+
 for text in reversed('world'):
-    print(text)
+    print(text, end='')
 #reverse
