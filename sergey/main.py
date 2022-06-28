aeac9dbcc4c38b1fff8178316330c7d0d28eec58
youshkoff <youshkoff@yandex.ru>
2022-06-28 12:05:58 +0300

    модифик мэйна

--- sergey/main.py
+++ sergey/main.py
@@ -1,5 +1,5 @@
 #from practice11 import all_positions
-from practice13 import get_string_pad
+from practice1 import get_string_pad
 from autotravel import print_check, all_positions
 
 print_check(all_positions)
