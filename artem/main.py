5139cc590b6ef260d22201ee37716d8044fa7cb0
Artem <a9113621595@gmail.com>
2022-06-30 20:02:32 +0300

    flaskproject

--- artem/main.py
+++ artem/main.py
@@ -1,18 +1,15 @@
-def reverse_word(text):
-    print(text[::-1])
+from practice1 import reverse_word as reverse
+from practice1 import frequency_sort
+from practice1 import low_to_up
 
 
 
 
- 
-def frequency_sort(items):
-    a = sorted(items,key=items.count,reverse = True)
-    return a
+reverse('artem')  # задача 1
 
 
+print(frequency_sort([2,3,5,3,7,9,5,3,7,3,5])) # задача 2
 
 
-def low_to_up(line):
-    data = iter(line)
-    return ''.join([x.lower() + y.upper() for x,y in zip(data,data)])
-    
+print(low_to_up(input())) # задача 3
+
