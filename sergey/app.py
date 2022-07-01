b82329c66c543f2c8380a2149f5695eaacf503a0
youshkoff <youshkoff@yandex.ru>
2022-07-01 18:24:02 +0300

    01.07 небольшая коррекция визитки

--- sergey/app.py
+++ sergey/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template  # из фласка импортируем класс(Flask)
+from flask import Flask, render_template  # из фласка импортируем класс(Flask), подключаем (рендрим) шаблоны из фласка
 
 app = Flask(__name__)  # делаем экземпляр класса
 
 @app.route("/")  # c помощью декоратора @app делаем зрительный образ, то что должно показывать на главной странице
-def index():
+def index():  # сопоставляем конкретный роутинг конкретной функции
     user_name = 'Сергей'
-    return render_template('index.html', user_name=user_name)
+    return render_template('index.html', user_name=user_name)  # возвращаем в главной функции результат выполнения функции рендер_темплэйт
 
 @app.route("/test")  # c помощью декоратора @app делаем зрительный образ, то что должно показывать странице
 def test():
