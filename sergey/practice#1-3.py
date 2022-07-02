1d779da52371f583059191c285e11093dd57d23d
Anton Popov <ademaro@ya.ru>
2022-07-02 13:57:41 +0530

    Чистка и реорганизация кода

--- sergey/practice#1-3.py
+++
@@ -1,7 +0,0 @@
-string = 'жаргон падонкаф'
-for i, l in enumerate(string):#пронумировали
-    if i%2:#четные нечетныеgit 
-        print(l.upper(), end='')
-    else:
-        print(l.lower(), end='')
-        
