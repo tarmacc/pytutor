1c011ec4259be080442a2dcaf39e870a3bbefbb5
Anton Popov <ademaro@ya.ru>
2022-07-15 17:06:55 +0530

    Добавлен модуль с инициализацией

--- anton/init.py
+++ anton/init.py
@@ -1,17 +1,20 @@
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
+from config import config
+from extensions import db, migrate, security
+from models import user_datastore
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config.from_object(config[os.getenv("FLASK_ENV", "production")])
 app.config.update(
-    SECRET_KEY="you-will-nejy$#gi#$s123",
     SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.join(basedir, 'app.db')}",
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db.init_app(app)
+migrate.init_app(app, db)
+# app, user_datastore, mail_util_cls=SecMailUtil
+security.init_app(app, user_datastore)
