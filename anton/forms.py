1470f03fbb11084642f6c148bd515ff767ff945c
Anton Popov <ademaro@ya.ru>
2022-07-18 17:27:15 +0530

    Рефакторинг

--- anton/forms.py
+++ anton/forms.py
@@ -1,19 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SubmitField, TextAreaField
+from wtforms import EmailField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 
 class DemoForm(FlaskForm):
     """Класс демо-формы."""
+
     name = StringField("Имя")
     last_name = StringField("Фамилия")
     email = EmailField(
-        "Email", validators=[
+        "Email",
+        validators=[
             DataRequired("Email обязателен для заполнения"),
-            Email("Email введён неправильно")
-        ]
+            Email("Email введён неправильно"),
+        ],
     )
-    text = TextAreaField("Сообщение", validators=[
-        DataRequired("Сообщение обязательно для заполнения")
-    ])
+    text = TextAreaField(
+        "Сообщение", validators=[DataRequired("Сообщение обязательно для заполнения")]
+    )
     submit = SubmitField("Отправить")
--- anton/init.py
+++ anton/init.py
@@ -1,20 +1,18 @@
 import os
 
-from flask import Flask
-
 from config import config
-from extensions import db, migrate, security
+from extensions import babel, db, executor, mail, migrate, security
+from flask import Flask
+from mail import SecMailUtil
 from models import user_datastore
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config.from_object(config[os.getenv("FLASK_ENV", "production")])
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.join(basedir, 'app.db')}",
-)
+babel.init_app(app)
 db.init_app(app)
+executor.init_app(app)
+mail.init_app(app)
 migrate.init_app(app, db)
-# app, user_datastore, mail_util_cls=SecMailUtil
-security.init_app(app, user_datastore)
+security.init_app(app, user_datastore, mail_util_cls=SecMailUtil)
